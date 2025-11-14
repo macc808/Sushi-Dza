80aa1234d72dbdde21376ffc66e9ed2d58647b0e
macc808 <157740469+macc808@users.noreply.github.com>
2025-11-14 19:04:30 +0200

    Add event listeners to cart buttons

--- main_script.js
+++ main_script.js
@@ -1,1 +1,12 @@
+const  add_cart_buttons = [...document.getElementsByClassName("add_to_cart")];
+buttons.forEach(add_to_cart => {
+  button.addEventListener("click", function() {
+    button.classList.toggle("addcart");
+    button.textContent = button.classList.contains("addcart") ? "Прибрати з кошика" : "Додати до кошика";
+  })
+});
 
+// for demo only
+setTimeout(function() {
+  document.getElementsByClassName("add_to_cart").focus();
+}, 500);
