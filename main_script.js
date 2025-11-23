7bb2ef017f3f57e1cf8fad65e53e5187964e85bd
macc808 <157740469+macc808@users.noreply.github.com>
2025-11-23 15:12:17 +0200

    Add random banner image functionality

--- main_script.js
+++ main_script.js
@@ -1,3 +1,24 @@
+var b2_txt = ""
+function random_banner2_img(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var random = Math.floor(Math.random() * (max - min + 1) + min);
+    if (random == 1) {
+        b2_txt = "https://i.postimg.cc/cC6T9bZz/rolls3.png"
+    }
+    if (random == 2) {
+        b2_txt= "https://i.postimg.cc/7YBLSRWK/Gemini-Generated-Image-gzf8tgzf8tgzf8tg-1.png"
+    }
+    if (random == 3) {
+        b2_txt = "https://i.postimg.cc/02tN74Bd/Gemini-Generated-Image-na26tvna26tvna26-1.png"
+    }
+    console.log(b2_txt);
+    var banner2_img = document.getElementById("banner2_change");
+    banner2_img.setAttribute('src' , b2_txt);
+}
+random_banner2_img(1, 3)
+    
+
 const buttons = [...document.getElementsByClassName("button")];
 buttons.forEach(button => {
   button.addEventListener("click", function() {
@@ -10,5 +31,3 @@ buttons.forEach(button => {
 setTimeout(function() {
   document.querySelector("button").focus();
 }, 500);
-
-
